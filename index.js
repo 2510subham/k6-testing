e5933ddecca7cd73b1d8f54c03d26c94d0878cb7
Subham Agarwal <subhamagarwal400@gmail.com>
2023-08-01 13:09:27 +0530

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+
+import http from 'k6/http';
+
+
+export const options = {
+    scenarios: {
+        constant_request_rate: {
+            executor: 'constant-arrival-rate',
+            rate: 1000,
+            timeUnit: '1s',
+            duration: '30s',
+            preAllocatedVUs: 100,
+            maxVUs: 100,
+        },
+    },
+};
+
+
+export default function () {
+    const url = process.env.URL;
+    const payload = JSON.stringify({
+        name: "hello",
+        age: 13
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    http.post(url, payload, params);
+}
